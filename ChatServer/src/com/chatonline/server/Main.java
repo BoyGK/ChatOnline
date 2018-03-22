7d89824a6397c37c9a95ac7aa366b1d19a1dfdef
caolu <chaolu@imudges.com>
2018-03-22 19:31:07 +0800

    增加slf4j依赖

--- ChatServer/src/com/chatonline/server/Main.java
+++ ChatServer/src/com/chatonline/server/Main.java
@@ -2,11 +2,13 @@ package com.chatonline.server;
 
 import com.chatonline.server.chat.ChatService;
 import com.chatonline.server.rpc.RpcService;
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 
 public class Main {
     private ChatService cService;
     private RpcService rService;
     public static void main(String[] args){
-
+        IoAcceptor acceptor = new NioSocketAcceptor();
     }
 }
