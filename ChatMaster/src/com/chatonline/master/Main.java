898b9ecbf0020ffb111449a5930d04b876494348
BoyGK <1215356195@qq.com>
2018-05-22 21:00:16 +0800

    by bgq

--- ChatMaster/src/com/chatonline/master/Main.java
+++ ChatMaster/src/com/chatonline/master/Main.java
@@ -7,15 +7,14 @@ import com.chatonline.master.upper.util.CreateTokenFactory;
 public class Main {
 
     public static void main(String... args){
-        UserDao dao = new UserDao();
-
-        User user = new User();
-        user.setId(2);
-        user.setUsername("bgq");
-        user.setPassword("654321");
-        user.setNickname("GK");
-        user.setToken(CreateTokenFactory.getRandomString());
-        dao.updata(user);
-        dao.close();
+//        UserDao dao = new UserDao();
+//        User user = new User();
+//        user.setId(2);
+//        user.setUsername("bgq");
+//        user.setPassword("654321");
+//        user.setNickname("GK");
+//        user.setToken(CreateTokenFactory.getRandomString());
+//        dao.updata(user);
+//        dao.close();
     }
 }
